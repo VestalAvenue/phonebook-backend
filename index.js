4535a79628849f4e86a96c4fde50730de31293e0
fadhil kabeer <fadhilkbr@gmail.com>
2025-08-18 07:48:30 +0000

    uibuild

--- index.js
+++ index.js
@@ -12,26 +12,10 @@ morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+    { "id": "1", "name": "Arto Hellas", "number": "040-123456" },
+    { "id": "2", "name": "Ada Lovelace", "number": "39-44-5323523" },
+    { "id": "3", "name": "Dan Abramov", "number": "12-43-234345" },
+    { "id": "4", "name": "Mary Poppendieck", "number": "39-23-6423122" }
 ]
 
 app.get('/api/persons', (request,response) => {
@@ -58,7 +42,7 @@ app.get('/api/persons/:id', (request,response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id // keep it as string
+    const id = request.params.id
     const person = persons.find(p => p.id === id)
 
     if (person) {
@@ -85,9 +69,13 @@ app.post('/api/persons/',(request,response) => {
     response.json(person)
 })
 
+const path = require('path')
 
+// 🔹 Catch-all route for React frontend
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
-
