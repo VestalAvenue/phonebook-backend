6cb6c2f5c3308467447da06c40aed887e8e639c2
VestalAve <fadhilkbr@gmail.com>
2025-08-20 14:35:11 +0530

    push

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ app.post('/api/persons/',(request,response) => {
 
 app.put('/api/persons/:id', (request, response, next) => {
   const body = request.body
-
+    console.log("updating id", request.params.id)
   const person = {
     name: body.name,
     number: body.number
